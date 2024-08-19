950c055c4d6d69016419c6ac36f55f2427e1b8dc
Frozen-H2O <43626458+Frozen-H2O@users.noreply.github.com>
2024-08-19 00:03:07 -0700

    Default dark/light mode to user's theme

--- gflbans/web/pages/__init__.py
+++ gflbans/web/pages/__init__.py
@@ -5,15 +5,14 @@ from starlette.requests import Request
 
 from gflbans.internal.config import BRANDING
 from gflbans.internal.flags import name2perms
-from gflbans.web.data import THEME
 from gflbans.internal.constants import GB_VERSION
 from gflbans.web.login import current_user
 
 
 async def sctx(request: Request) -> dict:
-    theme = THEME['light'] if 'dark_mode' not in request.session or not request.session['dark_mode'] else THEME['dark']
+    opposite_theme = True if 'opposite_theme' in request.session and request.session['opposite_theme'] else False
 
     user = await current_user(request)
 
-    return {'request': request, 'theme': theme, 'user': user, 'p_nodes': name2perms, 'GB_VERSION': GB_VERSION,
+    return {'request': request, 'opposite_theme': opposite_theme, 'user': user, 'p_nodes': name2perms, 'GB_VERSION': GB_VERSION,
             'GB_BEGIN_RENDER': datetime.now(tz=UTC).timestamp(), 'BRANDING': BRANDING}
